4db9b627e88f9486677d16093f0e41b3ed63cd70
BenTran <118144018+BenTran203@users.noreply.github.com>
2026-01-19 22:40:58 +0700

    Delete update_password.sql

--- update_password.sql
+++
@@ -1,8 +0,0 @@
-UPDATE users 
-SET password = '$2b$15$n0R86IR9tX4zmn/qCRqPueKK/XKK2YeRsCAhp0k2KyOYvupXViTWG'
-WHERE email = 'haxsol0983@gmail.com';
-
-SELECT email, "firstName", "lastName", "isEmailVerified" 
-FROM users 
-WHERE email = 'haxsol0983@gmail.com';
-
